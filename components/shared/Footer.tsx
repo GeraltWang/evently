ac6921ad9246ca9059f6279d56baa82dc24bfce4
GeraltWang <wgeralt@outlook.com>
2023-12-22 11:37:25 +0800

    feat: ✨ implement footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<footer className=' border-t'>
+			<div className=' flex-center wrapper flex-between flex gap-4 p-5 text-center flex-col sm:flex-row'>
+				<Link href={'/'}>
+					<Image src={'/assets/images/logo.svg'} width={128} height={38} alt='Evently logo' />
+				</Link>
+				<p>2024 Evently. All Rights reserved.</p>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
