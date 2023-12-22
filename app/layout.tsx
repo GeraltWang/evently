981e7477cc2e4511309427f0128a214b3841624b
GeraltWang <wgeralt@outlook.com>
2023-12-22 11:30:17 +0800

    feat: ✨ setup Clerk and Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 
 const poppins = Poppins({
@@ -18,8 +19,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang='en'>
-			<body className={poppins.className}>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang='en'>
+				<body className={poppins.className}>{children}</body>
+			</html>
+		</ClerkProvider>
 	)
 }
---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { headerLinks } from '@/constants'
+import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const NavItems = () => {
+	const pathname = usePathname()
+
+	return (
+		<ul className=' md:flex-between md:flex-row flex flex-col items-start gap-5  w-full'>
+			{headerLinks.map(link => {
+				const isActive = pathname === link.route
+
+				return (
+					<li
+						className={`${isActive && 'text-primary-500'} flex-center p-medium-16 whitespace-nowrap`}
+						key={link.route}
+					>
+						<Link href={link.route}>{link.label}</Link>
+					</li>
+				)
+			})}
+		</ul>
+	)
+}
+
+export default NavItems
