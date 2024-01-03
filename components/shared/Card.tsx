bc331581c6c9328355b8c7b2af79a29fecc600cf
GeraltWang <wgeralt@outlook.com>
2024-01-03 11:44:47 +0800

    fix: 🐛 hydration error

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -37,19 +37,21 @@ const Card = ({ event, hasOrderLink, hidePrice }: Props) => {
 					<DeleteConfirmation eventId={event.id} />
 				</div>
 			)}
-			<Link href={`/events/${event.id}`} className=' flex min-h-[230px] flex-col gap-3 p-5 md:gap-4'>
+			<div className=' flex min-h-[230px] flex-col gap-3 p-5 md:gap-4'>
 				{!hidePrice && (
 					<div className=' flex gap-2'>
 						<span className=' p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60'>
 							{event.isFree ? 'FREE' : `$${event.price}`}
 						</span>
-						<p className=' p-semibold-14 w-min rounded-full bg-gray-500/10 px-4 py-1 text-gray-500'>
+						<p className=' p-semibold-14 w-min rounded-full bg-gray-500/10 px-4 py-1 text-gray-500 line-clamp-1'>
 							{event.category.name}
 						</p>
 					</div>
 				)}
 				<p className=' p-medium-16 p-medium-18 text-gray-500'>{formatDateTime(event.startDateTime).dateTime}</p>
-				<p className=' p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black'>{event.title}</p>
+				<Link href={`/events/${event.id}`}>
+					<p className=' p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black'>{event.title}</p>
+				</Link>
 				<div className=' flex-between w-full'>
 					<p className=' p-medium-14 md:p-medium-16 text-gray-600'>
 						{event.organizer.firstName} {event.organizer.lastName}
@@ -61,7 +63,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: Props) => {
 						</Link>
 					)}
 				</div>
-			</Link>
+			</div>
 		</div>
 	)
 }
