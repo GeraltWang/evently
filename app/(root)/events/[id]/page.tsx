2432a481d660e1da2274dc4ba0407e8b01bde80e
GeraltWang <wgeralt@outlook.com>
2024-01-08 09:39:57 +0800

    style: 🎨 styling for image on Event Detail

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -20,13 +20,15 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
 		<>
 			<section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'>
 				<div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
-					<Image
-						src={event!.imageUrl}
-						alt='Hero Image'
-						width={1000}
-						height={1000}
-						className='h-full min-h-[300px] object-cover object-center'
-					/>
+					<div className='max-w-full'>
+						<Image
+							src={event!.imageUrl}
+							alt='hero image'
+							width={1000}
+							height={1000}
+							className='h-full min-h-[300px] object-cover object-center'
+						/>
+					</div>
 					<div className='flex w-full flex-col gap-8 p-5 md:p-10'>
 						<div className='flex flex-col gap-6'>
 							<h2 className='h2-bold'>{event?.title}</h2>
